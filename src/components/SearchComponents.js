792fe3e52df6161768920d25c81a3d3c1d943a9e
Srinivas <srinivasb1999@gmail.com>
2021-02-08 16:32:38 +0530

    fetch data using TMDB API and use useEffect()

--- src/components/SearchComponents.js
+++ src/components/SearchComponents.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchMovies() {
+
+    const [query, setQuery] = useState('');
+
+    const searchMovies = async (event) => {
+        event.preventDefault();
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=8de257bc1c81f081807fb95847926557&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <form className="form">
             <label className="label" htmlFor="query">Movie Name</label>
