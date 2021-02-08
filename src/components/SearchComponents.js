555611aa039c67ca535c9e7b16c7481c3047d499
Srinivas <srinivasb1999@gmail.com>
2021-02-08 18:31:08 +0530

    remove console log

--- src/components/SearchComponents.js
+++ src/components/SearchComponents.js
@@ -14,7 +14,6 @@ function SearchMovies() {
         try {
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data);
             setMovies(data.results);
         } catch (err) {
             console.error(err);
