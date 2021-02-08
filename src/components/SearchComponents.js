b9ea9d63c8ce6071175932ec88a9ba6b9f23c9e1
Srinivas <srinivasb1999@gmail.com>
2021-02-08 16:33:56 +0530

    make input tag a controlled component

--- src/components/SearchComponents.js
+++ src/components/SearchComponents.js
@@ -19,10 +19,15 @@ function SearchMovies() {
     };
 
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park" />
+            <input
+                className="input"
+                type="text" name="query"
+                placeholder="i.e. Jurassic Park"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+            />
             <button className="button" type="submit">Search</button>
         </form>
     );
