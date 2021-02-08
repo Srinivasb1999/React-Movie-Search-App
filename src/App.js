c79f40c90dbc94e02d6dcc78aa8bfcfa4cbfb6ac
Srinivas <srinivasb1999@gmail.com>
2021-02-08 16:01:27 +0530

    add SearchComponents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchComponents from './components/SearchComponents';
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">React Movie Search</h1>
+      <SearchComponents />
     </div>
   );
 }
