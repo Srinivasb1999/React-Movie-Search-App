2dc1ca127dc87c8b94f7f0f4f95d342e687cefc7
Srinivas <srinivasb1999@gmail.com>
2021-02-10 16:50:20 +0530

    prevent empty search

--- src/components/SearchComponents.js
+++ src/components/SearchComponents.js
@@ -11,13 +11,14 @@ function SearchMovies() {
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=8de257bc1c81f081807fb95847926557&language=en-US&query=${query}&page=1&include_adult=false`;
 
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            setMovies(data.results);
-        } catch (err) {
-            console.error(err);
-        }
+        if (query !== '')
+            try {
+                const res = await fetch(url);
+                const data = await res.json();
+                setMovies(data.results);
+            } catch (err) {
+                console.error(err);
+            }
     };
 
     return (
